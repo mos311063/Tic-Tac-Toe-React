bfaf0feda4d5fc24d1cbde4e445a865e9744fb16
Kardkasem Saenyen <meoss@Kardkasems-MacBook-Air.local>
2019-11-26 11:41:38 +1100

    Updates

---
+++ precache-manifest.689039763070a7816d207f26c472b19c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b74c9ad3650d8a35aa7c237c7db1424",
+    "url": "/Tic-Tac-Toe-React/index.html"
+  },
+  {
+    "revision": "2e34adc944601acf6698",
+    "url": "/Tic-Tac-Toe-React/static/css/main.a0c8184e.chunk.css"
+  },
+  {
+    "revision": "eb77d30066c648dcda29",
+    "url": "/Tic-Tac-Toe-React/static/js/2.aee9e266.chunk.js"
+  },
+  {
+    "revision": "2e34adc944601acf6698",
+    "url": "/Tic-Tac-Toe-React/static/js/main.b1eda5be.chunk.js"
+  },
+  {
+    "revision": "b2781f46ed4917aeaf0f",
+    "url": "/Tic-Tac-Toe-React/static/js/runtime-main.9fcf0acc.js"
+  }
+]);
